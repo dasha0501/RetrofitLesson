06407ce8403ba5f9d08e0de2e49ffcd1aca3c5d4
Weatherappppppp <ldvdasha@icloud.com>
2024-11-29 21:55:51 +0900

    Initial commit

---
+++ app/src/main/java/com/example/retrofitlesson/retrofit/MainApi.kt
@@ -0,0 +1,32 @@
+package com.example.retrofitlesson.retrofit
+
+import retrofit2.Response
+import retrofit2.http.Body
+import retrofit2.http.GET
+import retrofit2.http.Header
+import retrofit2.http.Headers
+import retrofit2.http.POST
+import retrofit2.http.Path
+import retrofit2.http.Query
+import retrofit2.http.QueryName
+
+interface MainApi {
+    @GET("auth/products/{id}")
+    suspend fun getProductById(@Path("id") id: Int): Product
+
+
+    @POST("auth/login")
+    suspend fun auth(@Body authRequest: AuthRequest): Response<User>
+
+    @Headers("Content-Type: application/json")
+    @GET("auth/products")
+    suspend fun getAllProducts(@Header("Authorization") accessToken: String): Products
+
+
+    @Headers("Content-Type: application/json")
+    @GET("auth/products/search")
+    suspend fun getProductsByNameAuth(@Header("Authorization") accessToken: String, @Query("q") name: String): Products
+
+    @GET("auth/products/search")
+    suspend fun getProductsByName(@Query("q") name: String): Products
+}
